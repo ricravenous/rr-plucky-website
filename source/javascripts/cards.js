1477bd9c95dc508fb976c467c2df4e7bb749a9d7
Ricky Rodriguez <ricky@rvnsc.com>
2018-09-21 16:02:06 -0400

    Add simple prompt app, organize stylesheets

---
+++ source/javascripts/cards.js
@@ -0,0 +1,11 @@
+cards = [
+ {
+ 	prompt: "How do we become more productive?"
+ },
+ {
+ 	prompt: "Name 3 morning activities."
+ },
+ {
+ 	prompt: "Name a favorite restaurant."
+ },
+]
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,27 @@
 // This is where it all goes :)
+
+cards = [
+ {
+ 	prompt: "How do we become more productive?"
+ },
+ {
+ 	prompt: "Name 3 morning activities."
+ },
+ {
+ 	prompt: "Name a favorite restaurant."
+ },
+]
+
+var cardIndex = 0;
+
+window.onload = function() {
+	var refreshButton = document.getElementById("refresh-button");
+	var cardPrompt = document.getElementById("card-prompt");
+
+	refreshButton.onclick = function() {
+		cardIndex = (cardIndex + 1) % cards.length;
+		cardPrompt.innerHTML = cards[cardIndex].prompt;
+	}
+}
+
+
