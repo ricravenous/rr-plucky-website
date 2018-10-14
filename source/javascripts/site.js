ba3e6f5bfb88b8fb08e6831d3885d0f577741442
ricravenous <ricky@rvnsc.com>
2018-10-14 16:48:09 -0400

    Fix toggle H1 bug on site. Make header H1 tags change on navigation click.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,26 +2,24 @@
 
 var cardIndex = 0;
 
-
-
 window.onload = function() {
-	var topicH1 = document.getElementById("card-topic-header");
+  var topicH1 = document.getElementById("card-topic-header");
 
-	var refreshButton = document.getElementById("refresh-button");
-	var cardPrompt = document.getElementById("card-prompt");
+  var refreshButton = document.getElementById("refresh-button");
+  var cardPrompt = document.getElementById("card-prompt");
 
-	function drawCard() {
-		cardIndex = (cardIndex + 1) % cards.length;
-		cardPrompt.innerHTML = cards[cardIndex].prompt;
-	}
+  function drawCard() {
+    cardIndex = (cardIndex + 1) % cards.length;
+    cardPrompt.innerHTML = cards[cardIndex].prompt;
+  }
 
-	refreshButton.onclick = drawCard;
+  refreshButton.onclick = drawCard;
 
-	//add listeners to the topic headers to change the card heading text
-	document.querySelectorAll(".topic__link").forEach(function(node) {
-		node.addEventListener('click', function(){
-			topicH1.innerText = node.innerText;
-			drawCard();
-		})
-	});
+  //add listeners to the topic headers to change the card heading text
+  document.querySelectorAll(".topics-nav__item").forEach(function(node) {
+    node.addEventListener('click', function(){
+      topicH1.innerText = node.innerText;
+      drawCard();
+    })
+  });
 }
