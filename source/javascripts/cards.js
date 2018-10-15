4d744cf1a26b2007e37da174438f34da74eeba2f
Ricky Rodriguez <35900496+ricravenous@users.noreply.github.com>
2018-10-15 13:21:06 -0400

    Variables and Clean Up
    Began adding variable definitions for color, typography, shadows, and placing them throughout project. Fixed up formatting, spacing, additional details. Added font files into the project for future PRs.

--- source/javascripts/cards.js
+++ source/javascripts/cards.js
@@ -1,14 +1,14 @@
 cards = [
- {
- 	prompt: "How do we become more productive?",
- 	topic: "productivity",
- },
- {
- 	prompt: "Name 3 morning activities.",
- 	topic: "management",
- },
- {
- 	prompt: "Name a favorite restaurant.",
- 	topic: "food",
- },
+{
+  prompt: "How do we become more productive?",
+  topic: "productivity",
+},
+{
+  prompt: "Name 3 morning activities.",
+  topic: "management",
+},
+{
+  prompt: "Name a favorite restaurant.",
+  topic: "food",
+},
 ];
