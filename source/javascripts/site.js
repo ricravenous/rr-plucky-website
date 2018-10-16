64752c12d956d594e85bd0357951a2c83b3165d0
Ricky Rodriguez <35900496+ricravenous@users.noreply.github.com>
2018-10-16 11:19:12 -0400

    Card Flip Function
    Add flip function to card on launch click, added styling to prompts, add sass information, move footer into card and delete old file. Fixed light formatting, and added cleaner z-index variables and use additional variables for spacing.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -9,19 +9,27 @@ window.onload = function() {
 
   var refreshButton = document.getElementById("refresh-button");
   var cardPrompt = document.getElementById("card-prompt");
+  var card = document.getElementById("card");
 
   function drawCard() {
-  cardIndex = (cardIndex + 1) % cards.length;
-  cardPrompt.innerHTML = cards[cardIndex].prompt;
-}
+    cardIndex = (cardIndex + 1) % cards.length;
+    cardPrompt.innerHTML = cards[cardIndex].prompt;
+    card.classList.add('card--transform');
 
-refreshButton.onclick = drawCard;
+  setTimeout(
+    function(){
+      card.classList.remove('card--transform'); }, 200
+    );
+  }
 
-//add listeners to the topic headers to change the card heading text
-document.querySelectorAll(".topic__link").forEach(function(node) {
-  node.addEventListener('click', function(){
-    topicH1.innerText = node.innerText;
-    drawCard();
-  })
-});
+  refreshButton.onclick = drawCard;
+
+  //add listeners to the topic headers to change the card heading text
+  document.querySelectorAll(".topic__link").forEach(function(node) {
+    node.addEventListener('click', function(card){
+      topicH1.innerText = node.innerText;
+      drawCard();
+      
+    })
+  });
 }
