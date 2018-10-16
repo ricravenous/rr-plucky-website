1dcd97be2ab67828b450452ada144895496e6a7a
ricravenous <ricky@rvnsc.com>
2018-10-16 12:57:04 -0400

    Fixed transform.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -9,27 +9,27 @@ window.onload = function() {
 
   var refreshButton = document.getElementById("refresh-button");
   var cardPrompt = document.getElementById("card-prompt");
-  var cardPrompt = document.getElementById("card");
+  var card = document.getElementById("card");
 
   function drawCard() {
-  cardIndex = (cardIndex + 1) % cards.length;
-  cardPrompt.innerHTML = cards[cardIndex].prompt;
-  card.classList.add('card--transform')
+    cardIndex = (cardIndex + 1) % cards.length;
+    cardPrompt.innerHTML = cards[cardIndex].prompt;
+    card.classList.add('card--transform');
 
   setTimeout(
-     function(){
-       card.classList.remove('card--transform'); }, 200
-     );
-   }
+    function(){
+      card.classList.remove('card--transform'); }, 200
+    );
+  }
 
+  refreshButton.onclick = drawCard;
 
-refreshButton.onclick = drawCard;
-
-//add listeners to the topic headers to change the card heading text
-document.querySelectorAll("[data-card-item]").forEach(function(node) {
-  node.addEventListener('click', function(){
-    topicH1.innerText = node.innerText;
-    drawCard();
-  })
-});
+  //add listeners to the topic headers to change the card heading text
+  document.querySelectorAll(".topic__link").forEach(function(node) {
+    node.addEventListener('click', function(card){
+      topicH1.innerText = node.innerText;
+      drawCard();
+      
+    })
+  });
 }
